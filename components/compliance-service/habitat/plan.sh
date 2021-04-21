f25d227b89f40c5e8e398eb7c16ace66655525df
Chef Expeditor <chef-ci@chef.io>
2021-04-21 20:34:18 +0000

    Bump inspec to 4.33.1/20210421070633
    This pull request was triggered automatically via Expeditor.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- components/compliance-service/habitat/plan.sh
+++ components/compliance-service/habitat/plan.sh
@@ -29,7 +29,7 @@ pkg_binds_optional=(
   [authn-service]="port"
   [notifications-service]="port"
 )
-inspec_release="chef/inspec/4.24.32/20210112215549"
+inspec_release="chef/inspec/4.33.1/20210421070633"
 pkg_deps=(
   core/coreutils
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
